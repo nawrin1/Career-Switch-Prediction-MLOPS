0d0e2cdd85013435c4b83a941151de59061b9e56
nawrin1 <jannatulnawrin@gmail.com>
2026-03-13 16:05:06 +0600

    remove embedded repository

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Jannatul Ferdous Nawrin",
+    author_email="jannatulnawrin@gmail.com",
+    packages=find_packages()
+)
